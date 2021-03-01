072c5e84efa593683c1bad08335ccea0ea0ffe52
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2021-03-01 12:09:28 -0600

    Change kitty opacity, among other things

--- .config/rofi/sidebar/scripts/launch.sh
+++ .config/rofi/sidebar/scripts/launch.sh
@@ -3,21 +3,17 @@ rofi_command="rofi -theme ~/.config/rofi/sidebar/launcher.rasi"
 
 # Links
 launcher=""
-browser=""
 screenshot=""
 adjusters=""
 sys=""
 
 # Variable passed to rofi
-options="$launcher\n$browser\n$screenshot\n$adjusters\n$sys"
+options="$launcher\n$screenshot\n$adjusters\n$sys"
 
 chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0)"
 case $chosen in
     $launcher)
         rofi -theme ~/.config/rofi/sidebar/apps.rasi -modi drun -show drun
-        ;;
-    $browser)
-        sh ~/.config/rofi/sidebar/scripts/browser.sh
         ;;
     $screenshot)
         sh ~/.config/rofi/sidebar/scripts/screenshot.sh
