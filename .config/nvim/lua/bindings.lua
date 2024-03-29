48cc40df444d868d54f8fd651333c92812cdff0c
sardonicism-04 <sardony01@gmail.com>
2024-03-28 21:09:59 -0400

    add nvim telescope

--- .config/nvim/lua/bindings.lua
+++ .config/nvim/lua/bindings.lua
@@ -4,3 +4,10 @@ vim.keymap.set('n', '<A-Left>', '<Cmd>BufferPrevious<CR>', {noremap = true})
 vim.keymap.set('n', '<A-Right>', '<Cmd>BufferNext<CR>', {noremap = true})
 vim.keymap.set('n', '<A-i>', '<Cmd>BufferPin<CR>')
 vim.keymap.set('n', '<A-q>', '<Cmd>BufferClose<CR>', {noremap = true})
+
+-- telescope bindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -61,6 +61,14 @@ return require('packer').startup(function(use)
 
         -- ** CODE TOOLS **
 
+        -- fuzzy finder
+        use {
+            'nvim-telescope/telescope.nvim', tag = '0.1.6',
+            -- or                            , branch = '0.1.x',
+            requires = { {'nvim-lua/plenary.nvim'} }
+        }
+        -- faster sorter code for fuzzy finder
+        use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
         -- treesitter
         use {
             'nvim-treesitter/nvim-treesitter',
